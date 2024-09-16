d00ed7b39960da6738bcd1bcfc9269e8ccde6218
GitHalex <calex1731@gmail.com>
2024-09-15 23:13:18 -0400

    Agregando autentiucacion

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException   # type: ignore
+from fastapi import FastAPI, Depends, HTTPException, status   # type: ignore
 from pydantic import BaseModel  # type: ignore
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm # type: ignore
 
@@ -7,7 +7,6 @@ app = FastAPI()
 
 # creamos instancia de nuestro sistema de autenticacion
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
-
 
 class User(BaseModel):
   username: str
@@ -38,18 +37,28 @@ users_db = {
 def search_user(username: str):
   if username in users_db:
     return UserDB(users_db[username])
+
+async def current_user(token: str = Depends(oauth2)):
+  user = search_user(token)
+  if not user:
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales de autenticacion invalidas", headers={"WWW-Authenticate": "Bearer"})
   
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
   user_db = users_db.get(form.username)
-  if not users_db:
-    raise HTTPException(status_code=400, detail="El usuario no es correcto")
+  if not user_db:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
   
   user = search_user(form.username)
   if not form.password == user.password:
-    raise HTTPException(status_code=400, detail="La contraseña no es correcta")
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
   
   return {
-    "access_token", 
+    "access_token": user.username, "token_type": "bearer"
   }
+  
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+  return user
+  
   
