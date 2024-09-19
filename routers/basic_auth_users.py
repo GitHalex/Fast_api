0b174e9bf2cc78afc305f79c78a1342f0a442266
GitHalex <calex1731@gmail.com>
2024-09-19 18:41:01 -0400

    Autenticacion de usuarios en la app

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -34,14 +34,24 @@ users_db = {
   }
 }
 
+  
+def search_user_db(username: str):
+  if username in users_db:
+    return UserDB(**users_db[username])
+
 def search_user(username: str):
   if username in users_db:
-    return UserDB(users_db[username])
+    return User(**users_db[username])
 
 async def current_user(token: str = Depends(oauth2)):
   user = search_user(token)
   if not user:
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales de autenticacion invalidas", headers={"WWW-Authenticate": "Bearer"})
+  
+  if user.disabled:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario inactivo")
+    
+  return user
   
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
@@ -49,7 +59,7 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
   if not user_db:
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
   
-  user = search_user(form.username)
+  user = search_user_db(form.username)
   if not form.password == user.password:
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
   
